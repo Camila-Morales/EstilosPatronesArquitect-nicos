4055beaa85c0327ec9717dbead579e0328e460cb
Camila Morales <87491325+Camila-Morales@users.noreply.github.com>
2024-11-25 20:44:29 -0500

    Add files via upload

---
+++ Composite/Component.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace RefactoringGuru.DesignPatterns.Composite.Conceptual
+{
+    public abstract class Component
+    {
+        public Component() { }
+        public abstract string Operation();
+        public virtual void Add(Component component)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Remove(Component component)
+        {
+            throw new NotImplementedException();
+        }
+        public virtual bool IsComposite()
+        {
+            return true;
+        }
+    }
+}
